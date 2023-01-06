715677b8a0947d80eb718d0caa1596bf39078ef6
unknown <victorgarrett@alunos.utfpr.edu.br>
2023-01-06 10:48:50 -0300

    servus connecti potest

--- server/src/connection/connection_handler.rs
+++ server/src/connection/connection_handler.rs
@@ -4,18 +4,19 @@ use async_trait::async_trait;
 
 #[async_trait]
 pub trait ConnectionHandler {
-    async fn on_connect(&self, address: String);
-    async fn on_disconnect(&self, address: String);
-    async fn on_message(&self, address: String, message: String);
+    async fn on_connect(&self, address: &String);
+    async fn on_disconnect(&self, address: &String);
+    async fn on_message(&self, address: &String, message: &String);
 }
 
+#[derive(Clone)]
 pub struct ConnectionHandlerImpl {
     pub tx: mpsc::Sender<(String, oneshot::Sender<()>)>,
 }
 
 #[async_trait]
 impl ConnectionHandler for ConnectionHandlerImpl {
-    async fn on_connect(&self, address: String) {
+    async fn on_connect(&self, address: &String) {
         let (tx, rx) = oneshot::channel();
         self.tx.send((format!("connect:{}", address), tx)).await.unwrap();
         tokio::spawn(async move {
@@ -23,7 +24,7 @@ impl ConnectionHandler for ConnectionHandlerImpl {
         });
     }
 
-    async fn on_disconnect(&self, address: String) {
+    async fn on_disconnect(&self, address: &String) {
         let (tx, rx) = oneshot::channel();
         self.tx.send((format!("disconnect:{}", address), tx)).await.unwrap();
         tokio::spawn(async move {
@@ -31,7 +32,7 @@ impl ConnectionHandler for ConnectionHandlerImpl {
         });
     }
 
-    async fn on_message(&self, address: String, message: String) {
+    async fn on_message(&self, address: &String, message: &String) {
         let (tx, rx) = oneshot::channel();
         self.tx.send((format!("message:{}:{}", address, message), tx)).await.unwrap();
         tokio::spawn(async move {
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,7 @@
 use tokio::net::TcpListener;
 use tokio::sync::*;
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
+
 
 mod connection;
 use connection::connection_handler;
@@ -10,33 +12,58 @@ async fn main() {
     let (tx, mut rx) = mpsc::channel(1024);
     let handler = connection_handler::ConnectionHandlerImpl { tx };
 
-    tokio::spawn(async move {
-        while let Some((event, tx)) = rx.recv().await {
-            let parts: Vec<&str> = event.split(":").collect();
-            let event_type = parts[0];
-            let address = parts[1];
+    println!("setting up listener");
+    let listener = TcpListener::bind("127.0.0.1:2345").await.unwrap();
 
-            match event_type {
-                "connect" => {
-                    println!("Client connected: {}", address);
-                }
-                "disconnect" => {
-                    println!("Client disconnected: {}", address);
-                }
-                "message" => {
-                    let message = parts[2];
-                    println!("Received message from {}: {}", address, message);
-                }
-                _ => {
-                    println!("Invalid event type: {}", event_type);
-                }
-            }
+    tokio::spawn(async move{
+        
+        loop{
+            println!("awaiting connection");
+            let (mut socket, addr) = listener.accept().await.unwrap();
+            let handler = handler.clone();
 
-            tx.send(()).unwrap();
+            tokio::spawn(async move {
+                let (socket_rx, mut socket_tx) = socket.split();
+                let mut socket_reader = BufReader::new(socket_rx);
+                let mut line = String::new();
+                handler.on_connect(&addr.to_string());
+
+                socket_tx.write("connected".as_bytes());
+
+                println!("socket setup");
+                println!("awaiting on socket");
+                while socket_reader.read_line(&mut line).await.unwrap() != 0{
+                    handler.on_message(&addr.to_string(), &line);
+                    socket_tx.write("message received".as_bytes());
+                }
+                println!("disconnected");
+                handler.on_disconnect(&addr.to_string());
+            }); 
         }
     });
 
-    handler.on_connect("127.0.0.1:8080".to_string());
-    handler.on_message("127.0.0.1:8080".to_string(), "Hello".to_string());
-    handler.on_disconnect("127.0.0.1:8080".to_string());
+    while let Some((event, tx)) = rx.recv().await {
+        let parts: Vec<&str> = event.split(":").collect();
+        let event_type = parts[0];
+        let address = parts[1];
+
+        match event_type {
+            "connect" => {
+                println!("Client connected: {}", address);
+            }
+            "disconnect" => {
+                println!("Client disconnected: {}", address);
+            }
+            "message" => {
+                let message = parts[2];
+                println!("Received message from {}: {}", address, message);
+            }
+            _ => {
+                println!("Invalid event type: {}", event_type);
+            }
+        }
+
+        tx.send(()).unwrap();
+    }
+
 }
