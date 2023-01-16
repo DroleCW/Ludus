08982bb0bc199a9b8a648e4f75740036b8016267
diogodsg <diogodasilvagouveia@gmail.com>
2023-01-16 07:42:30 -0300

    novum gambiarra factum est

--- game/src/core/states/main_menu_state/setup.rs
+++ game/src/core/states/main_menu_state/setup.rs
@@ -7,10 +7,21 @@ pub struct MenuData {
     menu_buttons: Entity,
     background: Entity,
 }
+#[derive(Component)]
+pub struct ButtonBox {
+    margin_top: f32,
+    margin_left: f32,
+}
 
 pub fn setup_menu(mut commands: Commands, asset_server: Res<AssetServer>) {
     let menu_buttons = commands
-        .spawn(layout::button_box())
+        .spawn((
+            layout::button_box(),
+            ButtonBox {
+                margin_top: 360.0,
+                margin_left: 80.0,
+            },
+        ))
         .with_children(|parent| button::transparent_button(parent, &asset_server, "Join game"))
         .with_children(|parent| button::transparent_button(parent, &asset_server, "Settings"))
         .with_children(|parent| button::transparent_button(parent, &asset_server, "Exit"))
@@ -34,13 +45,20 @@ pub fn setup_menu(mut commands: Commands, asset_server: Res<AssetServer>) {
 pub fn window_resized_event(
     mut events: EventReader<WindowResized>,
     mut background: Query<(&mut Transform, &mut Handle<Image>)>,
+    mut layout: Query<(&ButtonBox, &mut Style)>,
+    mut windows: ResMut<Windows>,
 ) {
+    let window = windows.primary_mut();
     for event in events.iter() {
         let width_scale = event.width / 1280.0;
         let height_scale = event.height / 720.0;
 
         for (mut transform, _) in &mut background {
             transform.scale = (width_scale, height_scale, 1.0).into();
+        }
+        for (button_box, mut style) in &mut layout {
+            style.margin.top = Val::Px(button_box.margin_top * (event.height / 720.0));
+            style.margin.left = Val::Px(button_box.margin_left * (event.width / 1280.0));
         }
     }
 }
